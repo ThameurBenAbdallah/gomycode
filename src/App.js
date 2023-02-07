3c234971781220fcd0901b4287e61819f93f3442
ThameurBenAbdallah <bathameur@gmail.com>
2023-02-07 21:52:01 +0100

    checkpoint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BasicExample from './compounents/forme';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+  <BasicExample/>
+    </div>
+  );
+}
+
+export default App;
